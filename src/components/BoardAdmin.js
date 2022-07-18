6aba73e1bceefe411e064c7099a35badf5b36128
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-07-18 06:59:06 -0400

    created board hooks for user roles

--- src/components/BoardAdmin.js
+++ src/components/BoardAdmin.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
 import UserService from "../services/user.service";
 
-const Home = () => {
+const BoardAdmin = () => {
     const [content, setContent]  = useState("");
 
     useEffect(() => {
-        UserService.getPublicContent().then(
+        UserService.getAdminBoard().then(
             (response) => {
                 setContent(response.data);
             },
             (error) => {
                 const _content = 
-                (error.response && error.response.data) ||
+                (error.response && error.response.data && error.response.data.message) ||
                 error.message ||
                 error.toString();
                 setContent(_content);
@@ -27,4 +27,4 @@ const Home = () => {
         </div>
     );
 };
-export default Home;
+export default BoardAdmin;
